3707af563a65078a06c6023e5f58b82be63f7b84
Alireza <a.razinejad@gmail.com>
2023-12-13 23:20:29 +0330

    feat(search): use the styled component for the input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,14 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const SearchComponent = styled.input`
+    border-radius: 0.5rem;
+    padding: 0.25rem;
+    border-color: #f3f5;
+    border-width: .1rem;
+    border-style: solid;
+    outline: none;
+`;
 
 export function Search() {
     const [query, setQuery] = useState('');
@@ -6,7 +16,7 @@ export function Search() {
     return (
         <>
             <p>Searched {query}</p>
-            <input type="search" onInput={(event) => setQuery((event.target as any).value)}/>
+            <SearchComponent type='search' onInput={(event) => setQuery((event.target as any).value)} />
         </>
     )
 }
