ae779c33829d2983cd189afe59f89ccadd57cee5
Alireza <a.razinejad@gmail.com>
2023-12-14 00:06:57 +0330

    style: initialized the content wrapper

--- src/pages/Today.tsx
+++ src/pages/Today.tsx
@@ -1,11 +1,20 @@
+import styled from 'styled-components';
+
 import { List } from '../components/List.tsx';
 import { Add } from '../components/Add.tsx';
 
+const ContentWrapper = styled.div`
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    flex-wrap: wrap;
+`;
+
 export const Today = () => {
     return (
-        <>
+        <ContentWrapper>
             <List />
             <Add />
-        </>
+        </ContentWrapper>
     )
 }
